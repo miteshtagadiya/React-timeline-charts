d5f754e31ff7e5418830c3755908e7cd24b3c62d
mitesh <miteshtagadiya@gmail.com>
2020-02-29 12:23:12 +0530

    fix: sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,59 @@
 /*** webpack.config.js ***/
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, "examples/src/index.html"),
-    filename: "./index.html"
+  template: path.join(__dirname, "examples/src/index.html"),
+  filename: "./index.html"
 });
 module.exports = {
-    entry: path.join(__dirname, "examples/src/index.js"),
-    output: {
-        path: path.join(__dirname, "examples/dist"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {}
-                  }
-                ]
-              }
+  entry: path.join(__dirname, "examples/src/index.js"),
+  output: {
+    path: path.join(__dirname, "examples/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
         ]
-    },
-    plugins: [htmlWebpackPlugin],
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    devServer: {
-        port: 3001
-    },
-    // externals: { 
-    //     // Don't bundle react or react-dom
-    //     'react': 'React',
-    //     'react-dom': 'ReactDOM',
-    //   }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    port: 3001
+  }
+  // externals: {
+  //     // Don't bundle react or react-dom
+  //     'react': 'React',
+  //     'react-dom': 'ReactDOM',
+  //   }
 };
