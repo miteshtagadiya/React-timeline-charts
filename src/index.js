02bda7fd4096fb42543f8525ce82733dbc60e88d
mitesh <miteshtagadiya@gmail.com>
2020-02-29 12:28:15 +0530

    fix:serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { default as App } from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
